b3e08b8cd83c55176bfa1c84809c50941c1f38f2
Fanyi Pu <FPU001@e.ntu.edu.sg>
2024-09-23 14:05:36 +0800

    Add .gitignore and download script for Kaggle dataset

---
+++ tools/data/download_from_kaggle.sh
@@ -0,0 +1,2 @@
+kaggle competitions download -c cassava-leaf-disease-classification -p .download
+unzip .download/cassava-leaf-disease-classification.zip -d .download/cassava-leaf-disease-classification
