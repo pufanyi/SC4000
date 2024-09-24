9739d6cc35d399a5908dffa335ca203e76a1645b
Fanyi Pu <FPU001@e.ntu.edu.sg>
2024-09-24 11:15:31 +0800

    Refactor data upload script and add support for generating datasets using Hugging Face library

--- tools/data/upload_to_hf.py
+++ tools/data/upload_to_hf.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from PIL import Image
 from tqdm import tqdm
-from datasets import Dataset, DatasetDict
+from datasets import Dataset, DatasetDict, features
 
 
 DATASET_PATH = ".download/cassava-leaf-disease-classification"
@@ -21,40 +21,40 @@ if __name__ == "__main__":
     with open(os.path.join(DATASET_PATH, "label_num_to_disease_map.json")) as f:
         label_map = json.load(f)
 
-    train_data = pd.read_csv(train_csv_path)
-    for i, row in tqdm(
-        train_data.iterrows(), total=len(train_data), desc="Processing train data"
-    ):
-        image_id = row["image_id"]
-        label = row["label"]
-        disease = label_map[str(label)]
-        path = os.path.join(train_path, image_id)
-        image = Image.open(path)
-        train.append(
-            {"image_id": path, "label": label, "image": image, "disease": disease}
-        )
+    def get_training_data():
+        train_data = pd.read_csv(train_csv_path)
+        for i, row in tqdm(
+            train_data.iterrows(), total=len(train_data), desc="Processing train data"
+        ):
+            image_id = row["image_id"]
+            label = row["label"]
+            disease = label_map[str(label)]
+            path = os.path.join(train_path, image_id)
+            image = Image.open(path)
+            yield {"image_id": image_id, "label": disease, "image": image}
 
-    test_data_path = os.listdir(test_path)
-    for id in tqdm(
-        test_data_path, total=len(test_data_path), desc="Processing test data"
-    ):
-        path = os.path.join(test_path, id)
-        image = Image.open(path)
-        test.append({"image_id": id, "image": image, "label": None, "disease": None})
+    def get_testing_data():
+        test_data_path = os.listdir(test_path)
+        for id in tqdm(
+            test_data_path, total=len(test_data_path), desc="Processing test data"
+        ):
+            path = os.path.join(test_path, id)
+            image = Image.open(path)
+            yield {"image_id": id, "image": image, "label": None}
 
-    features = {
-        "image_id": datasets.Value("string"),
-        "image": datasets.Image(),
-        "label": datasets.ClassLabel(
-            num_classes=len(label_map), names=list(label_map.keys())
-        ),
-        "disease": datasets.ClassLabel(
-            num_classes=len(label_map), names=list(label_map.values())
-        ),
-    }
+    features = features.Features(
+        {
+            "image_id": datasets.Value("string"),
+            "image": datasets.Image(),
+            "label": datasets.ClassLabel(
+                num_classes=len(label_map),
+                names=[label_map[k] for k in sorted(label_map.keys())],
+            ),
+        }
+    )
 
-    hf_train_dataset = Dataset.from_pandas(pd.DataFrame(train), features=features)
-    hf_test_dataset = Dataset.from_pandas(pd.DataFrame(test), features=features)
+    hf_train_dataset = Dataset.from_generator(get_training_data, features=features)
+    hf_test_dataset = Dataset.from_generator(get_testing_data, features=features)
     hf_dataset = DatasetDict({"train": hf_train_dataset, "test": hf_test_dataset})
-    hf_dataset.save_to_disk("./download/hf/cassava-leaf-disease-classification")
+    hf_dataset.save_to_disk(".download/hf/cassava-leaf-disease-classification")
     hf_dataset.push_to_hub("pufanyi/cassava-leaf-disease-classification")
