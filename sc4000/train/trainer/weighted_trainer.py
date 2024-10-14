6d5121a34487723b2764aabb879d966c69ab8f38
Fanyi Pu <FPU001@e.ntu.edu.sg>
2024-10-14 15:06:34 +0800

    reduce_lr_on_plateau

--- sc4000/train/trainer/weighted_trainer.py
+++ sc4000/train/trainer/weighted_trainer.py
@@ -2,6 +2,10 @@ import torch
 from transformers import Trainer, AdamW, Adafactor, get_scheduler
 from peft import LoftQConfig, LoraConfig, get_peft_model
 
+from sc4000.utils.logger import setup_logger
+
+logger = setup_logger(__name__)
+
 
 class WeightedTrainer(Trainer):
     def __init__(
@@ -10,7 +14,6 @@ class WeightedTrainer(Trainer):
         *args,
         weights,
         label_smoothing,
-        optimizer: str = "adafactor",
         scheduler: str = "reduce_lr_on_plateau",
         use_lora: bool = False,
         **kwargs,
@@ -24,28 +27,20 @@ class WeightedTrainer(Trainer):
         weights = [weights[w] for w in sorted(list(weights.keys()))]
         self.weight = torch.tensor(weights, dtype=torch.float32)
         self.label_smoothing = label_smoothing
-        self.optimizer_name = optimizer
         self.scheduler_name = scheduler
         super().__init__(model, *args, **kwargs)
 
-    def create_optimizer(self):
-        if self.optimizer_name == "adamw":
-            return AdamW(self.model.parameters(), lr=self.args.learning_rate)
-        elif self.optimizer_name == "adafactor":
-            return Adafactor(self.model.parameters(), lr=self.args.learning_rate, scale_parameter=False, relative_step=False)
-        else:
-            raise ValueError(f"Unknown optimizer: {self.optimizer_name}")
-
-    def create_scheduler(self, num_training_steps: int):
-        if self.scheduler_name == "reduce_lr_on_plateau":
-            return torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, mode='min', factor=0.1, patience=10)
-        else:
-            return get_scheduler(
-                self.scheduler_name,
-                optimizer=self.optimizer,
-                num_warmup_steps=self.args.warmup_steps,
-                num_training_steps=num_training_steps,
-            )
+    def create_scheduler(
+        self, num_training_steps: int, optimizer: torch.optim.Optimizer = None
+    ):
+        self.lr_scheduler = get_scheduler(
+            self.scheduler_name,
+            optimizer=self.optimizer if optimizer is None else optimizer,
+            num_warmup_steps=self.args.get_warmup_steps(num_training_steps),
+            num_training_steps=num_training_steps,
+            scheduler_specific_kwargs=self.args.lr_scheduler_kwargs,
+        )
+        return self.lr_scheduler
 
     def compute_loss(self, model, inputs, return_outputs=False):
         labels = inputs.get("labels")
