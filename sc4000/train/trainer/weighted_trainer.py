3ed81413f007db8ae8cc9e2c5fedd8ee554a9ca8
Fanyi Pu <FPU001@e.ntu.edu.sg>
2024-10-14 14:41:51 +0800

    Refactor dependencies in pyproject.toml

--- sc4000/train/trainer/weighted_trainer.py
+++ sc4000/train/trainer/weighted_trainer.py
@@ -1,12 +1,35 @@
 import torch
-from transformers import Trainer
+from transformers import Trainer, AdamW, Adafactor, get_scheduler
+from peft import LoftQConfig, LoraConfig, get_peft_model
 
 
 class WeightedTrainer(Trainer):
-    def __init__(self, *args, weights, label_smoothing, **kwargs):
+    def __init__(
+        self,
+        model,
+        *args,
+        weights,
+        label_smoothing,
+        optimizer: str = "adafactor",
+        scheduler: str = "reduce_lr_on_plateau",
+        use_lora: bool = False,
+        **kwargs,
+    ):
+        if use_lora:
+            self.loftq_config = LoftQConfig()
+            self.lora_config = LoraConfig(
+                init_lora_weights="loftq", loftq_config=self.loftq_config
+            )
+            self.model = get_peft_model(model, self.lora_config)
         weights = [weights[w] for w in sorted(list(weights.keys()))]
         self.weight = torch.tensor(weights, dtype=torch.float32)
         self.label_smoothing = label_smoothing
+        if optimizer == "adamw":
+            self.optimizer = AdamW
+        elif optimizer == "adafactor":
+            self.optimizer = Adafactor
+        else:
+            raise ValueError(f"Unknown optimizer: {optimizer}")
         super().__init__(*args, **kwargs)
 
     def compute_loss(self, model, inputs, return_outputs=False):
