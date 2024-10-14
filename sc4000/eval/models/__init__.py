c2b7d6b6985086f0108fb7591504ee12ba80e00e
Fanyi Pu <FPU001@e.ntu.edu.sg>
2024-10-14 13:27:04 +0800

    ConvNeXt Eval

--- sc4000/eval/models/__init__.py
+++ sc4000/eval/models/__init__.py
@@ -1,7 +1,8 @@
 from sc4000.eval.models.base import Model
 from sc4000.eval.models.vit import ViT
+from sc4000.eval.models.convnextv2 import ConvNeXtV2
 
-models = {"vit": ViT}
+models = {"vit": ViT, "convnextv2": ConvNeXtV2}
 
 
 def load_model(model_name: str, **kwargs):
