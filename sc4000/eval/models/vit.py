8cdf5ab9fa29736467c93b883be6c8c4e5e55fe4
Fanyi Pu <FPU001@e.ntu.edu.sg>
2024-10-24 22:03:35 +0800

    Refactor image preprocessing in ViT model

--- sc4000/eval/models/vit.py
+++ sc4000/eval/models/vit.py
@@ -3,6 +3,13 @@ from sc4000.eval.utils.results import Result
 
 from transformers import ViTImageProcessor, ViTForImageClassification
 from PIL import Image
+from torchvision.transforms import (
+    Compose,
+    Normalize,
+    ToTensor,
+    Resize,
+    CenterCrop,
+)
 
 
 class ViT(Model):
@@ -23,10 +30,23 @@ class ViT(Model):
             ignore_mismatched_sizes=True,
         ).to(self.device)
 
-    def predict(self, image: Image.Image) -> Result:
-        inputs = self.image_processor(image, return_tensors="pt").to(self.device)
-        outputs = self.model(**inputs)
-        logists_list = list(map(float, outputs.logits[0].detach().cpu().numpy()))
-        return Result(
-            prediction=outputs.logits.argmax(dim=-1).item(), logs=logists_list
+        self.image_mean, self.image_std = (
+            self.image_processor.image_mean,
+            self.image_processor.image_std,
         )
+        size = self.image_processor.size["height"]
+        normalize = Normalize(mean=self.image_mean, std=self.image_std)
+        self.test_transforms = Compose(
+            [
+                Resize(size),
+                CenterCrop(size),
+                ToTensor(),
+                normalize,
+            ]
+        )
+
+    def predict(self, image: Image.Image) -> Result:
+        inputs = self.test_transforms(image.convert("RGB")).unsqueeze(0).to(self.device)
+        outputs = self.model(inputs)
+        logits_list = outputs.logits[0].detach().cpu().numpy().tolist()
+        return Result(prediction=outputs.logits.argmax(dim=-1).item(), logs=logits_list)
