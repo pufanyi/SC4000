b38ef71ffb7c2dfe3c8438a3c3b4927bb77f1335
Fanyi Pu <FPU001@e.ntu.edu.sg>
2024-10-28 18:43:39 +0800

    Update dependencies in pyproject.toml

---
+++ tools/data/get_valset_res.sh
@@ -0,0 +1,7 @@
+python -m sc4000.eval.evaluate --model=convnextv2 --model_args=pretrained=pufanyi/SC4000_ConvNeXtV2_base_full_9000 --subset=full
+
+python -m sc4000.eval.evaluate --model=convnextv2 --model_args=pretrained=pufanyi/SC4000_ConvNeXtV2_base_balanced_12500 --subset=full
+
+python -m sc4000.eval.evaluate --model=vit --model_args=pretrained=pufanyi/SC4000_vit_large_full_15900 --subset=full
+
+python -m sc4000.eval.evaluate --model=mobilenetv3 --subset=full
